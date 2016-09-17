6ca7d8e363dec33a96706c237418e9cb25709408
anantharaju <anantharaju>
2016-09-17 12:48:14 +0530

    While Loop added

---
+++ WhileLoop/src/wlPackage/WhileLoop.java
@@ -0,0 +1,27 @@
+/**
+ * @author anantharaju
+ * Used to iterate a part of the program several times
+ * If the number of iteration is not fixed, it is recommended to use while loop
+ */
+package wlPackage;
+
+public class WhileLoop 
+{
+	public static void main(String[] args) 
+	{
+		int i=1;  
+		
+		//While Loop
+	    while(i<=10)
+	    {  
+	        System.out.println(i);  
+	        i++;  
+	    }  
+	    
+	    //Infinitive While Loop
+	    while(true)
+	    {  
+	        System.out.println("infinitive while loop");  
+	    }  
+	}
+}
